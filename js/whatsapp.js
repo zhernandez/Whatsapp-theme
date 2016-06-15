57540ffbd7e1b1a4d9723fc51ab9d815809e8d0d
zhernandez <jz.hdz23@gmail.com>
2016-06-15 08:26:07 -0500

    nivel uno :c

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,13 +1,24 @@
 function addRow() {
-    var li = document.createElement('li');
-
-    li.className = 'row';
-
-    //pasar lo del input a la variable nombre
-
-	var nombre = document.getElementById('intro').value;
-
-    li.innerHTML = nombre ;
-    
-     document.getElementById('content').appendChild(li);
+    var div = document.createElement('div');
+    div.className = 'mensaje-e';
+    var nombre = document.getElementById('intro');
+    div.innerHTML = nombre.value;
+    nombre.innerHTML = "";
+    document.getElementById('content').appendChild(div);
 }
+
+$(document).ready(function(){
+    $(".amigo").click(function(){
+      console.log("holi");
+      $(".op-j").empty();
+    });
+
+    $("#sendButton").click(function(){
+       var msg = $("#intro").val();
+       console.log(msg);
+       var divHead = '';
+       var divTail = '<span class="hora">11:14</span></h6></div>';
+       $('<div class="mensaje-e"><h6>'+msg+'<span class="hora">11:14</span></h6></div>').appendTo(".op-j");
+       $("#intro").val('');
+    });
+});
